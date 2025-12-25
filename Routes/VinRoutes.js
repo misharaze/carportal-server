c558db7c390c2451971dc8d70773193d5113cb44
mikhail guilbo <mikhailguilbo@MacBook-Pro-mikhail.local>
2025-12-25 02:25:43 +0100

    fix database import path

--- Routes/VinRoutes.js
+++ Routes/VinRoutes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { checkVinController } from "../Controller/vinController.js";
+import { checkVinController } from "../Controller/VinController.js";
 
 const router = Router();
 router.get("/:vin", checkVinController);
