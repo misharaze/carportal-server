bf75799efd7ec81293b54869b7a82e532ede424b
mikhail guilbo <mikhailguilbo@MacBook-Pro-mikhail.local>
2025-12-25 02:46:38 +0100

    fix database import path

--- Config/Database.js
+++ Config/Database.js
@@ -1,15 +1,14 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    dialect: "postgres",
-    port: process.env.DB_PORT,
-    logging: false
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
   }
-);
+});
 
 export default sequelize;
