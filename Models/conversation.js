6904818620fc3d80fade6ca43c5a387cee47c417
mikhail guilbo <mikhailguilbo@MacBook-Pro-mikhail.local>
2026-01-03 11:43:01 +0100

    добавленн чат и дороботаны модели

--- Models/conversation.js
+++ Models/conversation.js
@@ -1,21 +1,34 @@
 import { DataTypes } from "sequelize";
 
-
 export default function ConversationModel(sequelize) {
- const Conversation = sequelize.define("Conversation", {
+  const Conversation = sequelize.define("Conversation", {
     listingId: {
       type: DataTypes.INTEGER,
-      allowNull: false
+      allowNull: false,
+      field: "listingId"
     },
+
+    // в коде будет conversation.userAId,
+    // но в БД это колонка user1Id
     userAId: {
       type: DataTypes.INTEGER,
-      allowNull: false
+      allowNull: false,
+      field: "user1Id"
     },
+
+    // в коде будет conversation.userBId,
+    // но в БД это колонка user2Id
     userBId: {
       type: DataTypes.INTEGER,
-      allowNull: false
+      allowNull: false,
+      field: "user2Id"
     },
-    lastMessage: DataTypes.TEXT
+
+    lastMessage: {
+      type: DataTypes.TEXT,
+      field: "lastMessage"
+    }
   });
-  return Conversation
+
+  return Conversation;
 }
